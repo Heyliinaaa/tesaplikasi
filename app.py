19c3cde68c9c5f8a842965db1a4d3d87a23430c9
Heyliinaaa <166088182+Heyliinaaa@users.noreply.github.com>
2025-06-30 02:01:28 +0000

    Add main streamlit app File

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+# Daftar pertanyaan
+questions = [
+    {
+        "question": "1. Berapakah hasil dari 7 + 5?",
+        "options": ["10", "12", "13", "11"],
+        "answer": 1
+    },
+    {
+        "question": "2. Hewan berikut yang bisa terbang adalah ...",
+        "options": ["Kucing", "Burung", "Ikan", "Kuda"],
+        "answer": 1
+    },
+    {
+        "question": "3. Ibu kota Indonesia adalah ...",
+        "options": ["Bandung", "Surabaya", "Jakarta", "Yogyakarta"],
+        "answer": 2
+    },
+    {
+        "question": "4. Warna daun pada umumnya adalah ...",
+        "options": ["Merah", "Kuning", "Hijau", "Biru"],
+        "answer": 2
+    },
+    {
+        "question": "5. 10 buah apel dibagi 2 anak, masing-masing mendapat ...",
+        "options": ["2", "4", "6", "5"],
+        "answer": 3
+    },
+    {
+        "question": "6. Hari setelah Senin adalah ...",
+        "options": ["Selasa", "Rabu", "Kamis", "Jumat"],
+        "answer": 0
+    },
+    {
+        "question": "7. Sapi adalah hewan pemakan ...",
+        "options": ["Daging", "Rumput", "Ikan", "Serangga"],
+        "answer": 1
+    },
