c2ba67c39611fcbf358f5c714acec723c007d2d5
Heyliinaaa <166088182+Heyliinaaa@users.noreply.github.com>
2025-06-30 10:08:03 +0800

    app.py

--- app.py
+++ app.py
@@ -16,24 +16,5 @@ questions = [
         "question": "3. Ibu kota Indonesia adalah ...",
         "options": ["Bandung", "Surabaya", "Jakarta", "Yogyakarta"],
         "answer": 2
-    },
-    {
-        "question": "4. Warna daun pada umumnya adalah ...",
-        "options": ["Merah", "Kuning", "Hijau", "Biru"],
-        "answer": 2
-    },
-    {
-        "question": "5. 10 buah apel dibagi 2 anak, masing-masing mendapat ...",
-        "options": ["2", "4", "6", "5"],
-        "answer": 3
-    },
-    {
-        "question": "6. Hari setelah Senin adalah ...",
-        "options": ["Selasa", "Rabu", "Kamis", "Jumat"],
-        "answer": 0
-    },
-    {
-        "question": "7. Sapi adalah hewan pemakan ...",
-        "options": ["Daging", "Rumput", "Ikan", "Serangga"],
-        "answer": 1
-    },
+    }
+]
